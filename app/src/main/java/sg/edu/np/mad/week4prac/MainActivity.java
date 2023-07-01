70f2d5d343f2c06d263573c2e506b8408c604609
gwynethyeo723 <116245094+gwynethyeo723@users.noreply.github.com>
2023-07-01 15:29:01 +0800

    Update MainActivity.java

--- app/src/main/java/sg/edu/np/mad/week4prac/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/week4prac/MainActivity.java
@@ -2,48 +2,48 @@ package sg.edu.np.mad.week4prac;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    User u = new User();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("Debug", "create");
 
-        Intent rec = getIntent();
-        int value = rec.getIntExtra("id",0);
-        u = ListActivity.userList.get(value);
+        Button follow = findViewById(R.id.follow);
+        Button message = findViewById(R.id.message);
+        TextView name = findViewById(R.id.textView2);
+        TextView des = findViewById(R.id.textView);
 
-        TextView name = findViewById(R.id.txtName);
-        name.setText(u.getName());
-        TextView description = findViewById(R.id.txtDescription);
-        description.setText(u.getDesc());
-        setFollowBtn();
-    }
+        user user1 = (user) getIntent().getSerializableExtra("key");
+        name.setText(user1.name);
+        des.setText(user1.description);
+        follow.setText(user1.followed ? "unfollow" : "follow");
 
-    private void setFollowBtn() {
-        Button b = findViewById(R.id.btnFollow);
-        if(u.followed) {
-            b.setText("Unfollow");
-        }
-        else {
-            b.setText("Follow");
-        }
-    }
-    public void onFollowClick(View v) {
-        u.followed = !u.followed;
-        if(u.followed)
-            Toast.makeText(this, "Followed", Toast.LENGTH_SHORT).show();
-        else
-            Toast.makeText(this,"Unfollowed", Toast.LENGTH_SHORT).show();
-        setFollowBtn();
+        follow.setOnClickListener(new View.OnClickListener(){
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View view){
+                if (!user1.followed){
+                    follow.setText("unfollow");
+                    user1.followed = true;
+                    Toast.makeText(getApplicationContext(),
+                                    "followed",
+                                    Toast.LENGTH_SHORT)
+                            .show();
+                }
+                else {
+                    follow.setText("follow");
+                    user1.followed = false;
+                }
+            }
+
+        });
     }
 }
