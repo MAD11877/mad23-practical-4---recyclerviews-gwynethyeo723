0878eee831a13989c5f8ee5806ccd5e570bd2b19
gwynethyeo723 <s10222843@connect.np.edu.sg>
2023-07-01 15:59:12 +0800

    finished week 4

---
+++ app/src/main/java/sg/edu/np/mad/week4pracredo/ListActivity.java
@@ -0,0 +1,55 @@
+package sg.edu.np.mad.week4pracredo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class ListActivity extends AppCompatActivity {
+    int count = 1;
+    private static final String TAG = "ListActivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+
+
+        ArrayList<user> userlist = new ArrayList<>();
+        if (userlist.size() == 0){
+            for (int i = 0; i < 20; i++){
+                user newUser = createUser();
+                userlist.add(newUser);
+            }
+        }
+
+        RecyclerView recyclerView = findViewById(R.id.recycler);
+        adapter mAdapter = new adapter(ListActivity.this,userlist);
+        LinearLayoutManager mLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(mAdapter);
+
+    }
+    private int random(){
+        Random ran = new Random();
+        int value = ran.nextInt(999999999);
+        return value;
+    }
+    private user createUser(){
+        int ran1 = random();
+        int ran2 = random();
+        String name = "Name" + ran1;
+        boolean followed = false;
+        int id = count;
+        String description = "Description " + ran2;
+        count++;
+        user newUser = new user(name, description, id, false);
+        return newUser;
+    }
+}
