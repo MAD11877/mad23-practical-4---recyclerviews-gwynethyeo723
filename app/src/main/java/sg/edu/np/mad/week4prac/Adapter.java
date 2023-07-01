7ea7c660c642ed671ef837c9d378b4862e884033
gwynethyeo723 <116245094+gwynethyeo723@users.noreply.github.com>
2023-07-01 15:26:27 +0800

    Update Adapter.java

--- app/src/main/java/sg/edu/np/mad/week4prac/Adapter.java
+++ app/src/main/java/sg/edu/np/mad/week4prac/Adapter.java
@@ -1,85 +1,79 @@
 package sg.edu.np.mad.week4prac;
 
-import android.annotation.SuppressLint;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 
-public class Adapter extends RecyclerView.Adapter<myViewHolder>{
-    ArrayList<User> data;
-
-    public Adapter(ArrayList<User> input){
+public class adapter extends RecyclerView.Adapter<ViewHolder> {
+    ArrayList<user> data;
+    Context mContext;
+    AlertDialog.Builder builder;
+    public adapter (Context context, ArrayList<user> input){
         data = input;
-    }
-
-    @NonNull
-    @Override
-    public myViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View item = null;
-
-        if(viewType == 7) {
-            item = LayoutInflater.from(parent.getContext()).inflate(
-                    R.layout.reviewitem2,
-                    parent,
-                    false
-            );
-        } else {
-            item = LayoutInflater.from(parent.getContext()).inflate(
-                    R.layout.reviewitem,
-                    parent,
-                    false
-            );
-        }
-        return new myViewHolder(item);
+        mContext = context;
     }
 
     @Override
     public int getItemViewType(int position) {
-        return Integer.parseInt(data.get(position).name.substring(data.get(position).name.length()-1));
+        user s = data.get(position);
+        if (s.name.endsWith("7")) {
+            return 0;
+        }
+        return 1;
     }
 
+    @NonNull
     @Override
-    public void onBindViewHolder(@NonNull myViewHolder holder, @SuppressLint("RecyclerView") int position) {
-        User user = data.get(position);
-        holder.txtTitle.setText(user.name);
-        holder.txtDesc.setText(user.description);
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        if (viewType==0){
+            View item2 = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycler2, parent, false);
+            return new ViewHolder(item2);
+        }
+        View item1 = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycler, parent, false);
+        return new ViewHolder(item1);
+    }
 
-
-        holder.img.setOnClickListener(new View.OnClickListener(){
+    public void onBindViewHolder(ViewHolder holder, int position){
+        user s = data.get(position);
+        holder.txt.setText(s.name);
+        holder.txtdes.setText(s.description);
+        builder = new AlertDialog.Builder(mContext);
+        holder.image.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.d("Debug", "Image clicked");
-
-                new AlertDialog.Builder(holder.img.getContext())
-                        .setTitle("Profile")
-                        .setMessage(user.name)
-                        .setPositiveButton("View", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                Intent viewProfile = new Intent(holder.img.getContext(), MainActivity.class);
-                                viewProfile.putExtra("id", position);
-                                holder.img.getContext().startActivity(viewProfile);
+                builder.setTitle("Profile");
+                builder.setMessage(s.name);
+                builder.setPositiveButton(
+                        "VIEW",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                Intent myIntent = new Intent(mContext, MainActivity.class);
+                                myIntent.putExtra("key",s);
+                                mContext.startActivity(myIntent);
                             }
-                        })
-                        .setNegativeButton("Close", null)
-                        .show();
-
+                        });
+                builder.setNegativeButton(
+                        "Close",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }
+                        });
+                builder.show();
             }
         });
     }
 
-    @Override
-    public int getItemCount() {
+    public int getItemCount(){
         return data.size();
     }
 }
